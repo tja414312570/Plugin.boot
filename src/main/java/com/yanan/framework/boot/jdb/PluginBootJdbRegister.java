975773659225c1909c2f2ae1f07e7c5cafe4c8f3
Plugin <tja414312570@gmail.com>
2021-11-28 17:08:33 +0800

    jdb服务加了一点点东西

--- src/main/java/com/yanan/framework/boot/jdb/PluginBootJdbRegister.java
+++ src/main/java/com/yanan/framework/boot/jdb/PluginBootJdbRegister.java
@@ -1,22 +1,60 @@
 package com.yanan.framework.boot.jdb;
 
+import java.sql.SQLException;
+
 import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+
+import com.yanan.framework.jdb.datasource.DefaultDataSource;
 import com.yanan.framework.plugin.annotations.Register;
+import com.yanan.framework.plugin.annotations.Service;
 import com.yanan.framework.plugin.autowired.enviroment.Variable;
 
 @Variable("plugin.jdb")
 @Register(layInit = false)
 public class PluginBootJdbRegister {
+	@Service
+	private Logger logger;
+	
 	private String url;
 	
 	private String name;
 	
 	private String password;
+	//数据源ID
+	private String id;
+	
+	private String driver;
+	//框架本身属性
+	private int max_connection = 4;
+	private int min_connection = 2;
+	private int add_connection = 2;
+	private boolean test_connection = false;
+	private String test_sql = "";
+	private int wait_times;
+	private int login_timeout;
+
 	@PostConstruct
 	public void start() {
-		System.err.println(url);
-		System.err.println(name);
-		System.err.println(password);
-		throw new RuntimeException("");
+		logger.info("start plugin jdb service");
+		DefaultDataSource source = new DefaultDataSource();
+		source.setId(this.id);
+		source.setUrl(this.url);
+		source.setUsername(this.name);
+		source.setPassword(this.password);
+		source.setDriver(this.driver);
+		source.setMax_connection(max_connection);
+		source.setAdd_connection(add_connection);
+		source.setMin_connection(min_connection);
+		source.setTest_connection(test_connection);
+		source.setTest_sql(test_sql);
+		source.setWait_times(wait_times);
+		try {
+			source.setLoginTimeout(login_timeout);
+			source.init();
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
 	}
 }
