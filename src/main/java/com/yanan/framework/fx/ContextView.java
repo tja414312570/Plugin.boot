6b0f907ee7b488d40f1dc58bdcb54f46135605d3
Plugin <tja414312570@gmail.com>
2022-01-20 18:20:10 +0800

    fx支持

---
+++ src/main/java/com/yanan/framework/fx/ContextView.java
@@ -0,0 +1,18 @@
+package com.yanan.framework.fx;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 引导参数设置
+ * @author yanan
+ *
+ */
+@Retention(RUNTIME)
+@Target({ TYPE })
+public @interface ContextView {
+	String value() default "";
+}
